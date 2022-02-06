f34813874b97437604c53db6dfab91b28bbd5c98
ahmedwagih <ahmedxmen200@gmail.com>
2022-02-06 06:38:53 +0200

    navbar resize and responsive fix -- try2

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -18,7 +18,7 @@ function Menu() {
     return < div id="menu" className="container-fluid menu">
 
         <div className="row justify-content-center">
-            <h1 data-aos="fade-down" data-aos-duration="1000" style={{
+            <h1 data-aos="fade-down" data-aos-duration="2000" style={{
                 color: '#917c36', paddingTop: '100px',
                 marginLeft: '35px'
             }} className="text-center text-capitalize"> our menu </h1>
@@ -36,21 +36,23 @@ function Menu() {
             })
             }
 
-            <div className="row justify-content-center">
-                {arr2.map((img, ind) => {
-                    return <div data-aos="fade-up" data-aos-duration="1500" style={{margin: '25px'}}
-                                className="col-md-auto">
-                        <div className="card" style={{width: '250px', margin: '10px', color: '#917c36'}}>
-                            <img className="card-img-top" src={img} style={{height: "200px"}}/>
-                            <div className="card-body">
-                                <h5 className="card-title  text-center text-capitalize">{imgs2[ind]}</h5>
-                            </div>
+
+
+        </div>
+
+        <div className="row justify-content-center">
+            {arr2.map((img, ind) => {
+                return <div data-aos="fade-up" data-aos-duration="1500" style={{margin: '25px'}}
+                            className="col-md-auto">
+                    <div className="card" style={{width: '250px', margin: '10px', color: '#917c36'}}>
+                        <img className="card-img-top" src={img} style={{height: "200px"}}/>
+                        <div className="card-body">
+                            <h5 className="card-title  text-center text-capitalize">{imgs2[ind]}</h5>
                         </div>
                     </div>
-                })
-                }
-            </div>
-
+                </div>
+            })
+            }
         </div>
 
     </div>
