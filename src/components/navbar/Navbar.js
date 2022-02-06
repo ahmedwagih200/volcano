aac9cc564800e1f664d45e72d5d2ee884178b83e
ahmedwagih <ahmedxmen200@gmail.com>
2022-02-06 06:13:50 +0200

    navbar resize and responsive fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,17 +9,21 @@ import logo from '../imgs/stockLogo.jpeg';
 function Navbar() {
     AOS.init();
     return (
-        <div  data-aos="fade-right" data-aos-duration="1500"  className="d-flex justify-content-center sticky-top bg-light">
+
+    <div style={{height: "95px"}} data-aos="fade-right" data-aos-duration="1500" className="container-fluid sticky-top bg-light">
+        <div className="row justify-content-center ">
             <Navlink name="Home" val="Main"/>
-            <Navitem name="Menu" val="Main#menu" />
-            <div style={{ marginTop: "0px" , transform: 'scale(0.6)' }} className="navbar-header">
-                <img src={logo} />
+            <Navitem name="Menu" val="Main#menu"/>
+            <div style={{marginRight:"-50px", marginLeft:"-50px",  marginTop: "-20px", transform: 'scale(0.6)'}} className="col-md-auto navbar-header">
+                <img src={logo} alt="logo"/>
             </div>
-            <Navitem name="About" val="Main#about" />
-            <Navitem name="Contact" val="Main#contact" />
-            <Navlink name="Login" val="Empty/Login" />
+            <Navitem name="About" val="Main#about"/>
+            <Navitem name="Contact" val="Main#contact"/>
+            <Navlink name="Login" val="Empty/Login"/>
         </div>
+    </div>
+
     );
 }
-export default Navbar;
 
+export default Navbar;
