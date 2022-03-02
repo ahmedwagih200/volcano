a894145841cbbc4926a2ba7de9e4b4d1e5bdbe13
ahafeez96 <fezalm10@gmail.com>
2022-03-02 23:54:42 +0200

    add-to-cart

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import {BrowserRouter as Router, Route, Routes, Navigate, Outlet} from "react-router-dom";
-import Login from './components/forms/login';
-import ResetPassword  from './components/forms/ResetPassword';
-import ResetPasswordConfirm  from './components/forms/ResetPasswordConfirm';
-import Signup from './components/forms/Signup';
-import Activate from './components/forms/Activate';
-import Empty from './components/forms/Empty';
-import Main from './pages/Main'
-import Navbar from './components/navbar/Navbar'
-import './style.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import Login from "./components/forms/login";
+import ResetPassword from "./components/forms/ResetPassword";
+import ResetPasswordConfirm from "./components/forms/ResetPasswordConfirm";
+import Signup from "./components/forms/Signup";
+import Activate from "./components/forms/Activate";
+import Empty from "./components/forms/Empty";
+import Main from "./pages/Main";
+import Navbar from "./components/navbar/Navbar";
+import "./style.css";
 import "./components/footer/footer.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Provider} from 'react-redux';
-import store from './store';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import ChangePassword from "./components/forms/ChangePassword";
 import Profile from "./components/forms/Profile";
-
-
+import { CartProvider } from "react-use-cart";
+import Category from "./pages/category";
+import Cart from "./pages/Cart";
 function App() {
-    return (
-        <Provider store={store}>
+  return (
+    <Provider store={store}>
+      <CartProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="Main" element={<Main />} />
+            <Route path="category/:id" element={<Category />} />
 
-            <Router>
-                <Navbar/>
-                <Routes>
-                    <Route path="Main" element={<Main/>}/>
+            <Route path="Empty" element={<Empty />}>
+              <Route path="Signup" element={<Signup />} />
+              <Route path="Login" element={<Login />} />
+              <Route path="ResetPassword" element={<ResetPassword />} />
 
-                    <Route path="Empty" element={<Empty/>}>
+              <Route path="Profile" element={<Profile />} />
+              <Route path="ChangePassword" element={<ChangePassword />} />
+            </Route>
 
-                        <Route path="Signup" element={<Signup/>}/>
-                        <Route path="Login" element={<Login/>}/>
-                        <Route path="ResetPassword" element={<ResetPassword/>}/>
-
-                        <Route path="Profile" element={<Profile/>}/>
-                        <Route path="ChangePassword" element={<ChangePassword/>}/>
-                    </Route>
-
-                    <Route path="password/reset/confirm/:uid/:token" element={<ResetPasswordConfirm/>}/>
-                    <Route path='activate/:uid/:token' element={<Activate/>}/>
-                    <Route path="" element={<Navigate to="/Main"/>}/>
-                </Routes>
-            </Router>
-        </Provider>
-    );
+            <Route
+              path="password/reset/confirm/:uid/:token"
+              element={<ResetPasswordConfirm />}
+            />
+            <Route path="activate/:uid/:token" element={<Activate />} />
+            <Route path="" element={<Navigate to="/Main" />} />
+            <Route path="Cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </Provider>
+  );
 }
 
 export default App;
